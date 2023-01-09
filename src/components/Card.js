304ce113e56cebbd0b5f2a19c6c8646ff594bb4f
Umar Asif <umarasif737@gmail.com>
2023-01-10 00:15:50 +0500

    Code pushed

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ card }) => {
       <div className="image-container">
         <img className="carousal-image" src={card.imageSrc} alt="imga" />
       </div>
-      <div>
+      <div className="text-container">
         <div className="card-title">{card.title}</div>
         <div className="card-description">{card.description}</div>
       </div>
